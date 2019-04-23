f971b36a6854e41e643807578ac268b40e015ab5
jiangbaolin <jiangbaolin@yy.com>
2019-04-23 19:26:02 +0800

    修改单例，支持变参

--- singleton.h
+++ singleton.h
@@ -1,24 +1,21 @@
 #ifndef _SINGLE_TO_H_
 #define _SINGLE_TO_H_
-
+#include <memory>
 template<typename T>
 class SingleTon {
 public:
-    static T *getInstance();
+    template<typename... ARGS>
+    static std::shared_ptr<T> getInstance(ARGS... args) {
+        if(instance_ == nullptr) {
+            instance_ = std::shared_ptr<T>(new T(std::forward<ARGS>(args)...));
+        }
+        return instance_;
+    }
     virtual ~SingleTon();
 private:
     SingleTon();
-    static T *instance_;
+    static std::shared_ptr<T> instance_;
 };
-
-template<typename T>
-T *SingleTon<T>::getInstance()
-{
-    if(instance_ == nullptr) {
-        instance_ = new T;
-    }
-    return instance_;
-}
 
 template<typename T>
 SingleTon<T>::~SingleTon()
@@ -27,6 +24,6 @@ SingleTon<T>::~SingleTon()
 }
 
 template<typename T>
-T* SingleTon<T>::instance_ = nullptr;
+std::shared_ptr<T> SingleTon<T>::instance_ = nullptr;
 
 #endif
